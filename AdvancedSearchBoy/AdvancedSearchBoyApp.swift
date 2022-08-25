f11263899721ad15abe337e5b114bdb1268d3ac0
IKEH1024 <pondau1024@gmail.com>
2022-08-25 15:46:16 +0900

    initial commit

---
+++ AdvancedSearchBoy/AdvancedSearchBoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedSearchBoyApp.swift
+//  AdvancedSearchBoy
+//
+//  Created by HIROKI IKEUCHI on 2022/08/25.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedSearchBoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdvancedSearchBoy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdvancedSearchBoy
+//
+//  Created by HIROKI IKEUCHI on 2022/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
